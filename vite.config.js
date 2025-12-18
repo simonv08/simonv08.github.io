d6539b53a6bacc6dc0e01d963a9cb6bb284a99bb
Simon Vastenhoud <180269184+simonv08@users.noreply.github.com>
2025-12-18 09:35:30 +0100

    GitHub Pages deployment setup

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: "/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        404: "index.html",
-      },
-    },
-  },
+  plugins: [react()],
 });
